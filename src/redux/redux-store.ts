8fc600fa5201c97e87e3ba4d3a1db392ea9898eb
borashek <borashek@inbox.ru>
2023-03-04 20:00:02 +0300

    login

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,7 +1,6 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import {profileReducer} from "./profile-reducer";
 import {dialogsReducer} from "./dialogs-reducer";
-import {sidebarReducer} from "./sidebar-reducer";
 import {usersReducer} from "./users-reducer";
 import {authReducer} from "./auth-reducer";
 import {reducer as formReducer} from "redux-form";
@@ -11,7 +10,6 @@ let rootReducer = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   usersPage: usersReducer,
-  sidebar: sidebarReducer,
   auth: authReducer,
   form: formReducer
 })
--- src/redux/sidebar-reducer.ts
+++
@@ -1,8 +0,0 @@
-import {SidebarType} from "../../store";
-
-const initialState = {}
-
-export const sidebarReducer = (state: SidebarType = initialState, action: any) => {
-
-  return state
-}
