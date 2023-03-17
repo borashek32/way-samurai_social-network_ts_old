e7a6b032cc56b4a5e18956ff8891a0ff7bcfd3f2
borashek <borashek@inbox.ru>
2023-03-17 13:50:24 +0300

    optimization

--- src/components/content/profile/posts/Posts.tsx
+++ src/components/content/profile/posts/Posts.tsx
@@ -5,30 +5,80 @@ import {PostsPagePropsType} from "./PostsContainer";
 import {AddPostFormType, AddPostReduxForm} from "./AddPostForm";
 
 
-const Posts = React.memo<PostsPagePropsType>((props) => {
-  console.log('posts')
-  const addPost = (formData: AddPostFormType) => {
-    props.addPost(formData.descForNewPost)
+class Posts extends React.Component<PostsPagePropsType> {
+
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({a: 12})
+  //   }, 3000)
+  // }
+
+  shouldComponentUpdate(nextProps: Readonly<PostsPagePropsType>, nextState: Readonly<{}>, nextContext: any): boolean {
+    return nextProps === this.props && nextState === this.state
   }
 
-  return (
-    <div className={classes.posts}>
-      <h2 className={classes.posts__header}>{props.profilePage.title}</h2>
-      <div className={classes.addPost}>
-        <AddPostReduxForm onSubmit={addPost}/>
+  render() {
+    console.log('posts')
+
+    const addPost = (formData: AddPostFormType) => {
+      this.props.addPost(formData.descForNewPost)
+    }
+
+    return (
+      <div className={classes.posts}>
+        <h2 className={classes.posts__header}>{this.props.profilePage.title}</h2>
+        <div className={classes.addPost}>
+          <AddPostReduxForm onSubmit={addPost}/>
+        </div>
+        {this.props.profilePage.posts.map((p) => {
+          return (
+            <Post
+              likes={p.likes}
+              id={p.id}
+              desc={p.desc}
+              key={p.id}
+            />
+          )
+        })}
       </div>
-      {props.profilePage.posts.map((p) => {
-        return (
-          <Post
-            likes={p.likes}
-            id={p.id}
-            desc={p.desc}
-            key={p.id}
-          />
-        )
-      })}
-    </div>
-  );
-})
+    )
+  }
+}
 
 export default Posts
+
+
+// import classes from './Posts.module.sass'
+// import React from "react";
+// import {Post} from "./post/Post";
+// import {PostsPagePropsType} from "./PostsContainer";
+// import {AddPostFormType, AddPostReduxForm} from "./AddPostForm";
+
+
+// const Posts = React.memo<PostsPagePropsType>((props) => {
+//   console.log('posts')
+//   const addPost = (formData: AddPostFormType) => {
+//     props.addPost(formData.descForNewPost)
+//   }
+//
+//   return (
+//     <div className={classes.posts}>
+//       <h2 className={classes.posts__header}>{props.profilePage.title}</h2>
+//       <div className={classes.addPost}>
+//         <AddPostReduxForm onSubmit={addPost}/>
+//       </div>
+//       {props.profilePage.posts.map((p) => {
+//         return (
+//           <Post
+//             likes={p.likes}
+//             id={p.id}
+//             desc={p.desc}
+//             key={p.id}
+//           />
+//         )
+//       })}
+//     </div>
+//   );
+// })
+//
+// export default Posts
