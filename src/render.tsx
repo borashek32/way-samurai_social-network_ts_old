b1929ab1c19ff5f17dcb823a11273b9ffab991c3
borashek <borashek@inbox.ru>
2023-04-03 16:55:55 +0300

    github-pages

--- src/render.tsx
+++ src/render.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {store} from "./redux/redux-store";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter as BrowserRouter} from "react-router-dom";
 import { Provider } from 'react-redux';
 
 
